2239865717833e59692bb010418c7e0e460d2b2f
doDomoGu <kaka_71936410@qq.com>
2024-11-04 22:47:08 +0800

    修改文件生成的目录

--- calendar/index.js
+++ calendar/index.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
-import { readFile, writeFile } from "node:fs/promises";
+import { readFile, writeFile, mkdir,access } from "node:fs/promises";
+import path from 'path'
 
 console.time("runTime");
 let txt = "";
@@ -41,8 +42,22 @@ for (let i = 0; i < dayLength; i++) {
     `\n`;
 }
 
+async function checkAndCreateDir(dirPath) {
+  try {
+      // 检查目录是否存在
+      await access(dirPath);
+      // console.log(`目录 ${dirPath} 已存在`);
+  } catch {
+      // 目录不存在，创建目录
+      // console.log(`目录 ${dirPath} 不存在，创建它`);
+      await mkdir(dirPath, { recursive: true });
+  }
+}
+
 async function saveData(filePath, content) {
   try {
+    const dirPath = path.parse(filePath).dir
+    await checkAndCreateDir(dirPath)
     await writeFile(filePath, content); // 异步写入文件
     console.log("数据已保存到文件");
   } catch (error) {
@@ -50,6 +65,6 @@ async function saveData(filePath, content) {
   }
 }
 
-saveData("./data.txt", txt); // 调用函数
+saveData("./calendar/data/data.txt", txt); // 调用函数
 
 console.timeEnd("runTime");
