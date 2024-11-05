a636c9cda498ca2b6e1609f1f0bcc02672bc5118
dodomogu <dodomo_gu@sina.cn>
2024-11-05 20:55:10 +0800

    calendar json  

--- calendar/index.js
+++ calendar/index.js
@@ -1,30 +1,30 @@
 import dayjs from "dayjs";
-import { readFile, writeFile, mkdir,access } from "node:fs/promises";
-import path from 'path'
+import { readFile, writeFile, mkdir, access } from "node:fs/promises";
+import path from "path";
+import holiday from "./holiday.js";
 
 console.time("runTime");
-let txt = "";
+let dataArr = [];
 const startDate = dayjs("2021-01-01");
 
-const endDate = dayjs("2021-01-31");
-// const endDate = dayjs("2024-10-31");
+// const endDate = dayjs("2021-01-31");
+const endDate = dayjs();
 
 const dayLength = endDate.diff(startDate, "d") + 1;
 
 const weekdayMap = ["日", "一", "二", "三", "四", "五", "六"];
-const holiday = ["2021-01-01"];
 
 for (let i = 0; i < dayLength; i++) {
   const date = startDate.add(i, "day");
 
   const weekdayNum = date.format("d");
 
-  let isRight = true;
+  let isOpen = true;
   if (weekdayNum == 0 || weekdayNum == 6) {
-    isRight = false;
+    isOpen = false;
   }
   if (holiday.includes(date.format("YYYY-MM-DD"))) {
-    isRight = false;
+    isOpen = false;
   }
   //   console.log(holiday, date.format("YYYY-MM-DD"));
   //   console.log(
@@ -34,30 +34,35 @@ for (let i = 0; i < dayLength; i++) {
   //     `星期${weekdayMap[weekdayNum]}`,
   //     isRight ? "ok" : "not"
   //   );
-  txt +=
-    `date:\t` +
-    `${date.format("YYYY-MM-DD")}\t` +
-    `星期${weekdayMap[weekdayNum]}\t` +
-    `${isRight ? "ok" : "not"}\t` +
-    `\n`;
+  //   txt +=
+  //     `date:\t` +
+  //     `${date.format("YYYY-MM-DD")}\t` +
+  //     `星期${weekdayMap[weekdayNum]}\t` +
+  //     `${isOpen ? "ok" : "not"}\t` +
+  //     `\n`;
+  dataArr.push({
+    date: date.format("YYYY-MM-DD"),
+    weekday: `星期${weekdayMap[weekdayNum]}`,
+    isOpen,
+  });
 }
 
 async function checkAndCreateDir(dirPath) {
   try {
-      // 检查目录是否存在
-      await access(dirPath);
-      // console.log(`目录 ${dirPath} 已存在`);
+    // 检查目录是否存在
+    await access(dirPath);
+    // console.log(`目录 ${dirPath} 已存在`);
   } catch {
-      // 目录不存在，创建目录
-      // console.log(`目录 ${dirPath} 不存在，创建它`);
-      await mkdir(dirPath, { recursive: true });
+    // 目录不存在，创建目录
+    // console.log(`目录 ${dirPath} 不存在，创建它`);
+    await mkdir(dirPath, { recursive: true });
   }
 }
 
 async function saveData(filePath, content) {
   try {
-    const dirPath = path.parse(filePath).dir
-    await checkAndCreateDir(dirPath)
+    const dirPath = path.parse(filePath).dir;
+    await checkAndCreateDir(dirPath);
     await writeFile(filePath, content); // 异步写入文件
     console.log("数据已保存到文件");
   } catch (error) {
@@ -65,6 +70,6 @@ async function saveData(filePath, content) {
   }
 }
 
-saveData("./calendar/data/data.txt", txt); // 调用函数
-
+saveData("./calendar/data.json", JSON.stringify(dataArr)); // 调用函数
+console.log(dataArr.filter((n) => n.isOpen).length);
 console.timeEnd("runTime");
